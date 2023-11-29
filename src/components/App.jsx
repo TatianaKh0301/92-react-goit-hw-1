ae4e92f0a9ce30d3645ba706e1f7f0986e956176
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2023-11-29 17:58:00 +0200

    add user.json, Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import user from '../../src/user.json';
+import { Profile } from './Profile/Profile';
+
 export const App = () => {
   return (
     <div
@@ -10,7 +13,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile  username={user.username} avatar={user.avatar} tag={user.tag} location={user.location} stats={user.stats} />
     </div>
   );
 };
