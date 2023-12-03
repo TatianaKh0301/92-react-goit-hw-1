0843a32666bc76f966ba8e26a2b1d5c5b8e5a1ee
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2023-12-03 14:21:42 +0200

    Update Profile.styled.jsx

--- src/components/Profile/Profile.styled.jsx
+++ src/components/Profile/Profile.styled.jsx
@@ -1,4 +1,5 @@
-import styled from 'styled-components';
+import styled from '@emotion/styled';
+
 
 export const ProfileWrap = styled.div`
     position: relative;
@@ -8,11 +9,10 @@ export const ProfileWrap = styled.div`
     background-color: rgb(210, 210, 247);
     width: 100vw;
     height: 605px
-`; 
+`;
 
 export const ProfileInfo = styled.div`
-  
-position: absolute;              
+    position: absolute;              
     top: 50%;  
     left: 50%;                      
     transform: translate(-50%, -50%);
@@ -23,7 +23,7 @@ position: absolute;
     box-shadow: 0.5px 0.5px 0.5px 0.5px rgb(197, 194, 194);
 `;
 
-export const Description = styled.div` 
+export const Description = styled.div`
     padding: 40px;
     background-color: white;
     border-top-left-radius: 3%;
@@ -32,18 +32,80 @@ export const Description = styled.div`
     border:none;
 `;
 
-export const Avatar = styled.img`
+export const ProfileAvatar = styled.img`
     display: block;
     margin: 0 auto;
     height: 120px;
     width: 120px;
     border-radius: 50%;
     background-color: gray;
-    margin-bottom: 30px;        display: block;
-    margin: 0 auto;
-    height: 120px;
-    width: 120px;
-    border-radius: 50%;
-    background-color: gray;
     margin-bottom: 30px;        
+`;
+
+export const TextWrap = styled.div`
+    padding-top: 5px;
+    padding-bottom: 5px;
+    text-align: center;
+`;
+
+export const Name = styled.p`
+    font-size: 20px;
+    font-weight: bold;
+    color: black;
+    margin-bottom: 5px;
+`;
+
+export const Tag = styled.p`
+    margin-bottom: 5px;
+    color: gray;
+    font-size: 15px;
+`;
+
+export const Location = styled.p`
+    color: gray;
+    font-size: 15px;
+`;
+
+export const Stats = styled.ul`
+    display: flex;
+    width: 100%;
+    height: 79px;
+    list-style: none;
+`;
+
+export const StatsItem = styled.li`
+    border: 0.01px solid rgb(197, 194, 194);
+    width: 100px;
+    position: relative;
+    border-bottom: none;
+
+    &:first-of-type {
+        border-bottom-left-radius: 3%;
+        border-left: none;
+    }
+
+    &:last-child {
+        border-bottom-right-radius: 3%;
+        border-right: none;
+    }
+`;
+
+export const StatsItemContainer = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`;
+
+export const Quantity = styled.span`
+    display: block;
+    text-align: center;
+    color:black;
+    font-size: 18px;
+    font-weight: bold;
+`;
+
+export const Label = styled.span`
+    color: rgb(70, 67, 67);
+    font-size: 18px;  
 `;
