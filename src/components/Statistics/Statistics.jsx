ea5a6784f931b388817870527db38004aa70b88e
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2023-12-04 17:19:57 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,26 +1,21 @@
-export const Statistics = ({stats}) => {
-    const {id, label, persentage} = stats;
+export const Statistics = ({title, stats}) => {
     return (
         <section class="statistics">
             <h2 class="title">Upload stats</h2>
             <ul class="stat-list">
-                <li class="item">
-                    <span class="label">.docx</span>
-                    <span class="percentage">4%</span>
-                </li>
-                <li class="item">
-                    <span class="label">.mp3</span>
-                    <span class="percentage">14%</span>
-                </li>
-                <li class="item">
-                    <span class="label">.pdf</span>
-                    <span class="percentage">41%</span>
-                </li>
-                <li class="item">
-                    <span class="label">.mp4</span>
-                    <span class="percentage">12%</span>
-                </li>
+                {
+                    stats.map(data => (
+                        <li key={data.id} class="item" style={{backgroundColor: `${getRandomHexColor()}`}}>
+                            <span class="label">{data.label}</span>
+                            <span class="percentage">{data.persentage}</span>
+                        </li>
+                    ))
+                }              
             </ul>
         </section>
     );
 };
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
