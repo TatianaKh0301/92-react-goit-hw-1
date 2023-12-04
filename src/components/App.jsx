800f0f3945a4536bd4b64ec95cdab459651b2cda
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2023-12-04 17:59:07 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,14 +9,21 @@ export const App = () => {
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
       }}
     >
-      <Profile  username={user.username} avatar={user.avatar} tag={user.tag} location={user.location} stats={user.stats} />
-      <Statistics id={data.id} label={data.label} persentage={data.persentage}/>
+      <Profile  
+          username={user.username} 
+          avatar={user.avatar} 
+          tag={user.tag} 
+          location={user.location} 
+          stats={user.stats} 
+      />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
